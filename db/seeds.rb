2635c2dab0f9c5db0a648bea245d82fb59a8ecf6
silentfl <silentfl@mail.ru>
2017-05-04 19:40:30 +0300

    Add seeds (with faker gem)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# create teacher
+teacher = User.create!(
+  email: 'teacher@example.com',
+  password: '123456'
+)
+teacher.add_role(:teacher)
+
+#create students
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: '123456'
+  )
+end
+
+# create courses
+10.times do
+  Course.create!(
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+  )
+end
